82b07a74043177a03b4cc67e8892c578cf0465c4
Brian Rosner <brosner@gmail.com>
2008-02-09 22:50:50 +0000

    Fixed #6 and #7. Refactored command handling and improved the who command. This also adds a very basic help command. who now accepts multiple nicknames. Thanks, akaihola for the good ideas.
    git-svn-id: http://djangobot.googlecode.com/svn/trunk@18 78c5c301-0d45-0410-a974-35d49dbb1d3e

--- djangobot.py
+++ djangobot.py
@@ -19,8 +19,79 @@ class HTTPHeadRequest(urllib2.Request):
     def get_method(self):
         return "HEAD"
 
+class IRCMessage(object):
+    def __init__(self, irc, user, channel, message):
+        self.irc = irc
+        self.user = user.split("!", 1)[0]
+        self.channel = channel
+        self.message = message
+    
+    def parse_as_command(self):
+        try:
+            cmd, params = self.message.split(" ", 1)
+        except ValueError:
+            cmd, params = self.message, ()
+        else:
+            params = params.split(" ")
+        self.resolve_command(cmd)(*params)
+    
+    def resolve_command(self, cmd):
+        try:
+            cmd_func = getattr(self, "cmd_%s" % cmd)
+        except AttributeError:
+            return self.unknown_cmd
+        else:
+            return cmd_func
+    
+    def parse_as_normal(self):
+        if self.message.startswith(self.irc.nickname + ":"):
+            self.irc.msg(self.channel, "%s: i am a bot. brosner is my creator." % self.user)
+        # find any referenced tickets in this message
+        # this requires the syntax #1000 to trigger.
+        tickets = re.findall(r"(?:^|[\s(])#(\d+)\b", self.message)
+        for ticket in tickets:
+            url = "http://code.djangoproject.com/ticket/%s" % ticket
+            if self.check_url(url):
+                self.irc.msg(self.channel, url)
+        # find changesets. requires r1000 syntax.
+        changesets = re.findall(r"\br(\d+)\b", self.message)
+        for changeset in changesets:
+            url = "http://code.djangoproject.com/changeset/%s" % changeset
+            if self.check_url(url):
+                self.irc.msg(self.channel, url)
+    
+    def check_url(self, url):
+        try:
+            urllib2.urlopen(HTTPHeadRequest(url))
+        except urllib2.HTTPError:
+            return False
+        else:
+            return True
+    
+    def unknown_cmd(self, *params):
+        self.irc.msg(self.user, "unknown command")
+    
+    def cmd_help(self, *params):
+        self.irc.msg(self.user, "available commands: who")
+    
+    def cmd_who(self, *nicknames):
+        for nickname in [n.strip() for n in nicknames]:
+            try:
+                u = urllib2.urlopen("http://djangopeople.net/api/irc_lookup/%s/" % nickname)
+            except urllib2.HTTPError:
+                self.irc.msg(self.user, "something went wrong!")
+            else:
+                response = u.read()
+                if response == "no match":
+                    self.irc.msg(self.user, "%s was not found." % nickname)
+                else:
+                    self.irc.msg(self.user, "%s is %s" % (nickname, response))
+    cmd_who.usage = ""
+    cmd_who.help_text = ""
+
 class DjangoBotProtocol(irc.IRCClient):
     def connectionMade(self):
+        self.in_channel = False
         self.nickname = self.factory.nickname
         self.password = self.factory.password
         irc.IRCClient.connectionMade(self)
@@ -45,47 +116,17 @@ class DjangoBotProtocol(irc.IRCClient):
         """
         Once the bot has signed into the channel begin the trac updates.
         """
+        self.in_channel = True
         task.LoopingCall(self.trac_updates, channel).start(15)
     
     def privmsg(self, user, channel, message):
-        user = user.split("!", 1)[0]
-        if channel == self.nickname:
-            try:
-                cmd, params = message.split(" ", 1)
-            except ValueError:
-                # no params
-                cmd = message
-            if cmd == "who":
-                try:
-                    u = urllib2.urlopen("http://djangopeople.net/api/irc_lookup/%s/" % params.strip())
-                except http.HTTPError:
-                    self.msg(user, "something went wrong!")
-                else:
-                    self.msg(user, u.read())
-            return
-        if message.startswith(self.nickname + ":"):
-            self.msg(channel, "%s: i am a bot. brosner is my creator." % user)
-        # find any referenced tickets in this message
-        # this requires the syntax #1000 to trigger.
-        tickets = re.findall(r"(?:^|[\s(])#(\d+)\b", message)
-        for ticket in tickets:
-            url = "http://code.djangoproject.com/ticket/%s" % ticket
-            if self.check_url(url):
-                self.msg(channel, url)
-        # find changesets. requires r1000 syntax.
-        changesets = re.findall(r"\br(\d+)\b", message)
-        for changeset in changesets:
-            url = "http://code.djangoproject.com/changeset/%s" % changeset
-            if self.check_url(url):
-                self.msg(channel, url)
-    
-    def check_url(self, url):
-        try:
-            urllib2.urlopen(HTTPHeadRequest(url))
-        except urllib2.HTTPError:
-            return False
-        else:
-            return True
+        if self.in_channel:
+            msg = IRCMessage(self, user, channel, message)
+            # TODO: make this a case-insenstive comparsion
+            if channel == self.nickname:
+                msg.parse_as_command()
+            else:
+                msg.parse_as_normal()
 
 class DjangoBotService(service.Service):
     def __init__(self, channel, nickname, password):
