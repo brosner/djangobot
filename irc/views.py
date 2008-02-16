5c916eff96a8dc9d3a50bbadffe01ae72a234669
Brian Rosner <brosner@gmail.com>
2008-02-16 17:12:39 +0000

    Fixed the ordering of the last 100 message in the logger.

--- irc/views.py
+++ irc/views.py
@@ -8,5 +8,5 @@ def channel_detail(request, channel_name):
     channel = get_object_or_404(Channel, name="#%s" % channel_name)
     return render_to_response("irc/channel_detail.html", {
         "channel": channel,
-        "messages": channel.message_set.all()[:100],
+        "messages": reversed(channel.message_set.order_by("-logged")[:100]),
     }, context_instance=RequestContext(request))
