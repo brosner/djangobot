8d50292121ad19bb19e54a107328a698de188f55
Brian Rosner <brosner@gmail.com>
2008-02-17 05:06:33 +0000

    Made sure the channel_detail view never gets cached since it always has new content.
    git-svn-id: http://djangobot.googlecode.com/svn/trunk@37 78c5c301-0d45-0410-a974-35d49dbb1d3e

--- irc/views.py
+++ irc/views.py
@@ -1,5 +1,6 @@
 
 from django.template import RequestContext
+from django.views.decorators.cache import never_cache
 from django.shortcuts import get_object_or_404, render_to_response
 
 from irc.models import Channel
@@ -10,3 +11,4 @@ def channel_detail(request, channel_name):
         "channel": channel,
         "messages": reversed(channel.message_set.order_by("-logged")[:100]),
     }, context_instance=RequestContext(request))
+channel_detail = never_cache(channel_detail)
