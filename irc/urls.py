097bcb5828d8071aaff6f9efc6e3ba82f000ead8
Brian Rosner <brosner@gmail.com>
2008-02-20 21:56:24 +0000

    Added an index page for the logger.
    git-svn-id: http://djangobot.googlecode.com/svn/trunk@41 78c5c301-0d45-0410-a974-35d49dbb1d3e

--- irc/urls.py
+++ irc/urls.py
@@ -1,8 +1,13 @@
 
 from django.conf.urls.defaults import *
+from django.views.generic.list_detail import object_list
 
 from irc.views import *
+from irc.models import Channel
 
 urlpatterns = patterns("",
-    url(r"^(?P<channel_name>[-\w]+)/$", channel_detail),
+    url(r"^(?P<channel_name>[-\w]+)/$", channel_detail, name="channel_detail"),
+    url(r"^$", object_list, {
+        "queryset": Channel.objects.all(),
+    }),
 )
