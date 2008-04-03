48478d286935e5c3f556524b369f5c5329a4e0d2
Brian Rosner <brosner@gmail.com>
2008-04-03 17:29:48 +0000

    Explicitly set the timezone in manage.py due to the use of settings.configure.

--- manage.py
+++ manage.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+import os
+import time
+
 from ConfigParser import SafeConfigParser
 
 config = SafeConfigParser()
@@ -9,10 +13,14 @@ from django.conf import settings
 settings.configure(**{
     "DATABASE_ENGINE": config.get("db", "engine"),
     "DATABASE_NAME": config.get("db", "name"),
+    "TIME_ZONE": "UTC",
     "INSTALLED_APPS": (
         "irc",
     ),
 })
+
+os.environ["TZ"] = settings.TIME_ZONE
+time.tzset()
 
 from django.core.management import execute_from_command_line
 
