0d5e5e2a5269cd1cca015d53a355ed67d9ae8930
Brian Rosner <brosner@gmail.com>
2008-04-12 03:18:17 +0000

    Fixed up a couple other bugs. query was not defined. The get_messages queryset was stored in a local variable that caused the DEBUG page to evalulate it.
    git-svn-id: http://djangobot.googlecode.com/svn/trunk@75 78c5c301-0d45-0410-a974-35d49dbb1d3e

--- logger/views.py
+++ logger/views.py
@@ -18,8 +18,8 @@ def get_messages(channel, query=""):
 
 def channel_detail(request, channel_name):
     channel = get_object_or_404(Channel, name="#%s" % channel_name)
-    messages = get_messages(channel, request.GET.get("q", ""))
-    paginator = QuerySetPaginator(messages, settings.PAGINATE_BY)
+    query = request.GET.get("q", "")
+    paginator = QuerySetPaginator(get_messages(channel, query), settings.PAGINATE_BY)
     try:
         page_number = int(request.GET.get("page", 1))
     except ValueError:
